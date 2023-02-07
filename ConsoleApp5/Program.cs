a630fe1aedb1be256b7aab679d8e65b52d5898a7
muradpunisher <muradsm@code.edu.az>
2023-02-07 07:40:28 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Notebook
+    {
+        public string Brand;
+        public int Model;
+        public int Price;
+        
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} - Model: {Model} - Price: {Price}");
+        }
+
+    }
+
+    internal class Program
+    {
+        static void main(string[] args)
+        {
+            Notebook ntb1 = new Notebook()
+            {
+                Brand = "Lenovo",
+                Model = 30437863,
+                Price = 690,
+                
+            };
+        }
+    }
+    
+}
---
+++ ConsoleApp5/student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Student
+    {
+        
+        public string FullName;
+        public int Point;
+        public int GroupNo;
+        public bool IsPassed;
+        public Student(byte point) 
+        {
+            Point= point;
+        }
+    }
+    internal class student
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(65);
+
+            student.FullName = "Totu Qafqazli";
+            student.GroupNo = '7';
+            {
+                if (student.Point>65) 
+                {
+                    student.IsPassed= true;
+                    Console.WriteLine("kifayetdir");
+                    
+                };
+            }
+
+        }
+    }
+    
+}
